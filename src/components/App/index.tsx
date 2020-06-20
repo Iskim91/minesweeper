648c343735ca5a2966d9839faf249b2095a62b47
Isac Kim <jk724814@gmail.com>
2020-06-20 15:26:54 +0100

    minesweep using React typescript tutorial

---
+++ src/components/App/index.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+import NumberDisplay from '../NumberDisplay';
+import Button from '../Button';
+
+import {generateCells} from '../../utils';
+
+import "./App.scss";
+
+const App: React.FC = () => {
+    const [cells, setCells] = useState(generateCells());
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) => row.map((cell, colIndex) =>  <Button key={`${rowIndex}=${colIndex}`}/>))
+    }
+    return (
+        <div className="App">
+            <div className="Header">
+                <NumberDisplay value={0}/>
+                <div className="Face">
+                    <span role="img" aria-label="face">😎</span>
+                </div>
+                <NumberDisplay value={23}/>
+            </div>
+            <div className="Body">
+                {renderCells()}
+            </div>
+        </div>
+    );
+}
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Button.scss';
+
+interface Props {
+
+}
+
+const Button: React.FC<Props> = () => {
+    return (
+        <div className="Button">
+
+        </div>
+    );
+};
+
+export default Button;
